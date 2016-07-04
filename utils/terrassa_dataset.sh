e485dbb9da86a07fd1db5b19c265a8c51d5351f0
Manel <manelbaradad@gmail.com>
2016-07-04 21:20:17 +0200

    First version of terrassa script + gitignore

---
+++ utils/terrassa_dataset.sh
@@ -0,0 +1,4 @@
+mkdir -p ../datasets
+mkdir -p ../datasets/terrassa
+wget  https://imatge.upc.edu/web/sites/default/files/projects/1634/public/terrassa-buildings/terrassa900-trainval.zip -O ../datasets/terrassa/terrassa900-trainval.zip
+wget  https://imatge.upc.edu/web/sites/default/files/projects/1634/public/terrassa-buildings/terrassa900-test.zip -O ../datasets/terrassa/terrassa900-test.zip
