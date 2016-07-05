d0acb3229485cc32c09505450d861485fb8d59cc
Manel <manelbaradad@gmail.com>
2016-07-05 14:17:29 +0200

    Preliminary terrassa dataset, non-working

--- utils/datasets/terrassa.py
+++ utils/datasets/terrassa.py
@@ -6,12 +6,12 @@ import os
 import subprocess
 import os.path
 import csv
+from scipy import misc
 
-def load_data():
-  path = "../datasets/terrassa/terrassa.pkl"
+def load_data(path="../../datasets/terrassa", download=True):
 
   #if file doesn't exist, create download and create the pickle, so next is fast
-  if(os.path.isfile(path)):
+  if(os.path.isfile(path + "/terrassa.pickle")):
      f = open(path, 'rb')
      data = pickle.load(f)
      f.close()
@@ -19,37 +19,43 @@ def load_data():
 
   else:
     print("Getting the dataset. This can't take a while.")
-    execute_script()
+    if(download):
+      execute_script()
     print("Dataset downloaded.")
 
-    X_train, y_train =  load_folder("../datasets/terrassa/TerrassaBuildings900/train/")
-    X_train, y_train = load_folder("../datasets/terrassa/TerrassaBuildings900/val/")
-    X_test, y_test = load_folder("../datasets/terrassa/TerrassaBuildings900/val/")
+    X_train, y_train =  load_folder(path + "/TerrassaBuildings900/train")
+    X_val, y_val = load_folder(path +  "/TerrassaBuildings900/val")
+    X_test, y_test = load_folder(path +  "/test")
 
     f = open(path, "wb")
-    data = [(X_train, y_train),(X_test,y_test)]
+    data = (X_train, y_train),(X_val,y_val), (X_test,y_test)
     pickle.dump(data, f)
     f.close()
     return data
 
 
 def execute_script():
-  FNULL = open(os.devnull, 'w')
-  subprocess.call('./terrassa_dataset.sh')
+  subprocess.call(['./terrassa_dataset.sh'])
 
 def load_folder(path):
-  path += "/images"
   if (os.path.isfile(path + "/annotation.txt")):
-    csv_reader = csv.reader(open('text.txt', 'rb'), delimiter='\t')
+    csv_reader = csv.reader(open(path + "/annotation.txt"), delimiter='\t')
     #skip first line
     next(csv_reader)
-    annotations = list(csv_reader)
-
-
-
+    annotations = dict(csv_reader)
+  x = []
+  y = []
+  path += "/images"
+  for fn in os.listdir(path):
+    if os.path.isfile(path + "/" + fn):
+      x.append(annotations[os.path.splitext(fn)[0]])
+      y.append(misc.imread(path + "/" + fn))
+      print (fn)
+  return x,y
 
 if __name__ == "__main__":
-  load_data()
+  load_data(download=False)
 #  (X_train, y_train), (X_test, y_test) = mnist.load_data()
+
 
 
--- utils/datasets/terrassa_dataset.sh
+++ utils/datasets/terrassa_dataset.sh
@@ -1,6 +1,8 @@
-mkdir -p ../datasets
-mkdir -p ../datasets/terrassa
-wget  https://imatge.upc.edu/web/sites/default/files/projects/1634/public/terrassa-buildings/terrassa900-trainval.zip -O ../datasets/terrassa/terrassa900-trainval.zip
-wget  https://imatge.upc.edu/web/sites/default/files/projects/1634/public/terrassa-buildings/terrassa900-test.zip -O ../datasets/terrassa/terrassa900-test.zip
-unzip terrassa900-test.zip -d test
-unzip terrassa900-train.zip -d train
+#!/bin/bash
+mkdir -p ../../datasets
+mkdir -p ../../datasets/terrassa
+#wget  https://imatge.upc.edu/web/sites/default/files/projects/1634/public/terrassa-buildings/terrassa900-trainval.zip -O ../../datasets/terrassa/terrassa900-trainval.zip
+#wget  https://imatge.upc.edu/web/sites/default/files/projects/1634/public/terrassa-buildings/terrassa900-test.zip -O ../../datasets/terrassa/terrassa900-test.zip
+cd ../../datasets/terrassa/
+unzip -o terrassa900-test.zip -d test
+unzip -o terrassa900-trainval.zip 
