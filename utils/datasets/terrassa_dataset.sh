26930fab36cea1acbb011e3cdabafdf81b6c04ec
Manel <manelbaradad@gmail.com>
2016-07-05 21:23:44 +0200

    Uncommented download

--- utils/datasets/terrassa_dataset.sh
+++ utils/datasets/terrassa_dataset.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir -p ../../datasets
 mkdir -p ../../datasets/terrassa
-#wget  https://imatge.upc.edu/web/sites/default/files/projects/1634/public/terrassa-buildings/terrassa900-trainval.zip -O ../../datasets/terrassa/terrassa900-trainval.zip
-#wget  https://imatge.upc.edu/web/sites/default/files/projects/1634/public/terrassa-buildings/terrassa900-test.zip -O ../../datasets/terrassa/terrassa900-test.zip
+wget  https://imatge.upc.edu/web/sites/default/files/projects/1634/public/terrassa-buildings/terrassa900-trainval.zip -O ../../datasets/terrassa/terrassa900-trainval.zip
+wget  https://imatge.upc.edu/web/sites/default/files/projects/1634/public/terrassa-buildings/terrassa900-test.zip -O ../../datasets/terrassa/terrassa900-test.zip
 cd ../../datasets/terrassa/
 unzip -o terrassa900-test.zip -d test
 unzip -o terrassa900-trainval.zip 
