7936ddfcd0359de4b005b36f591874747521d7f4
miriambellver <miriambellver93@gmail.com>
2016-07-05 21:50:37 +0200

    Terrassa dataset to vgg

--- task4/vgg16_finetunning.py
+++ task4/vgg16_finetunning.py
@@ -1,5 +1,4 @@
 from keras.optimizers import SGD
-from convnetskeras.convnets import preprocess_image_batch, convnet
 from keras.models import Sequential
 from keras.layers.core import Flatten, Dense, Dropout
 from keras.datasets import cifar10, mnist
@@ -7,6 +6,8 @@ from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2
 from keras.layers.normalization import BatchNormalization
 from keras.utils import np_utils
 from keras.callbacks import ModelCheckpoint
+from utils.datasets.terrassa import terrassa 
+
 
 nb_classes=10
 batch_size = 128
@@ -14,35 +15,35 @@ nb_epoch = 5
 data_augmentation = False
 
 
-def preprocess_image_batch(image_paths, img_size=None, crop_size=None, color_mode="rgb", out=None):
-    img_list = []
+# def preprocess_image_batch(image_paths, img_size=None, crop_size=None, color_mode="rgb", out=None):
+#     img_list = []
     
-    for im_path in image_paths:
-        img = imread(im_path, mode='RGB')
-        if img_size:
-            img = imresize(img,img_size)
+#     for im_path in image_paths:
+#         img = imread(im_path, mode='RGB')
+#         if img_size:
+#             img = imresize(img,img_size)
             
-        img = img.astype('float32')
-        # We permute the colors to get them in the BGR order
-        if color_mode=="bgr":
-            img[:,:,[0,1,2]] = img[:,:,[2,1,0]]
-        # We normalize the colors with the empirical means on the training set
-        img[:, :, 0] -= 123.68 
-        img[:, :, 1] -= 116.779
-        img[:, :, 2] -= 103.939
-        img = img.transpose((2, 0, 1))
+#         img = img.astype('float32')
+#         # We permute the colors to get them in the BGR order
+#         if color_mode=="bgr":
+#             img[:,:,[0,1,2]] = img[:,:,[2,1,0]]
+#         # We normalize the colors with the empirical means on the training set
+#         img[:, :, 0] -= 123.68 
+#         img[:, :, 1] -= 116.779
+#         img[:, :, 2] -= 103.939
+#         img = img.transpose((2, 0, 1))
 
-        if crop_size:
-            img = img[:,(img_size[0]-crop_size[0])//2:(img_size[0]+crop_size[0])//2
-                      ,(img_size[1]-crop_size[1])//2:(img_size[1]+crop_size[1])//2]
+#         if crop_size:
+#             img = img[:,(img_size[0]-crop_size[0])//2:(img_size[0]+crop_size[0])//2
+#                       ,(img_size[1]-crop_size[1])//2:(img_size[1]+crop_size[1])//2]
             
-        img_list.append(img)
+#         img_list.append(img)
 
-    img_batch = np.stack(img_list, axis=0)
-    if not out is None:
-        out.append(img_batch)
-    else:
-        return img_batch
+#     img_batch = np.stack(img_list, axis=0)
+#     if not out is None:
+#         out.append(img_batch)
+#     else:
+#         return img_batch
 
 def VGG_16(weights_path=None):
 
@@ -102,6 +103,21 @@ def VGG_16(weights_path=None):
 
 # model = convnet('alexnet',weights_path="./weights/alexnet_weights.h5", heatmap=False, trainable=False)
 
+(X_train, y_train), (X_test, y_test) = terrassa.load_data()
+
+print('X_train shape:', X_train.shape)
+print(X_train.shape[0], 'train samples')
+print(X_test.shape[0], 'test samples')
+
+Y_train = np_utils.to_categorical(y_train, nb_classes)
+Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+
+
 model=VGG_16("./weights/vgg16_weights.h5")
 # model = convnet('VGG_16',weights_path="./weights/vgg16_weights.h5", heatmap=False)
 
@@ -118,20 +134,6 @@ model.add(layer_last)
 
 sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(optimizer=sgd, loss='mse', metrics=['accuracy'])
-
-(X_train, y_train), (X_test, y_test) = cifar10.load_data()
-
-print('X_train shape:', X_train.shape)
-print(X_train.shape[0], 'train samples')
-print(X_test.shape[0], 'test samples')
-
-Y_train = np_utils.to_categorical(y_train, nb_classes)
-Y_test = np_utils.to_categorical(y_test, nb_classes)
-
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
-X_train /= 255
-X_test /= 255
 
 checkpointer = ModelCheckpoint(filepath="./temporal_weights/weights_finetunned_alexnet_terrassa.hdf5", verbose=1, save_best_only=True)
 
