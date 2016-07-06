30278c1285bf5a58101657907fd1df0ed810610f
miriambellver <miriambellver93@gmail.com>
2016-07-06 11:46:26 +0200

    modification to cifar finetunning

--- task4/cifar10_cnn_finetunning.py
+++ task4/cifar10_cnn_finetunning.py
@@ -20,12 +20,12 @@ from keras.layers import Convolution2D, MaxPooling2D
 from keras.optimizers import SGD
 from keras.utils import np_utils
 from keras.callbacks import ModelCheckpoint
-import terrassa
+from utils.datasets import terrassa
 
 batch_size = 32
 nb_classes = 10
 nb_epoch = 200
-data_augmentation = True
+data_augmentation = False
 
 # input image dimensions
 img_rows, img_cols = 32, 32
@@ -35,8 +35,6 @@ img_channels = 3
 weights_path="./temporal_weights/weights_cifar.hdf5"
 
 # the data, shuffled and split between train and test sets
-(X_train, y_train), (X_test, y_test) = cifar10.load_data()
-
 # (X_train2, y_train2), (X_test2, y_test2) = cifar10.load_data()
 
 print('X_train shape:', X_train.shape)
@@ -96,6 +94,17 @@ sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy',
               optimizer=sgd,
               metrics=['accuracy'])
+
+
+(X_train, y_train), (X_test, y_test) = terrassa.load_data()
+
+print('X_train shape:', X_train.shape)
+print(X_train.shape[0], 'train samples')
+print(X_test.shape[0], 'test samples')
+
+nb_classes = len(set(y_test))
+Y_train = np_utils.to_categorical(y_train, nb_classes)
+Y_test = np_utils.to_categorical(y_test, nb_classes)
 
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
