8b379fac7d9caa024a1d926309e1cfa9c8c96b3e
hect1995 <hect.esteban@gmail.com>
2016-07-06 12:58:42 +0200

    Update mnist_cnn.py

--- task2/mnist_cnn.py
+++ task2/mnist_cnn.py
@@ -42,16 +42,27 @@ Y_test = np_utils.to_categorical(y_test, nb_classes)
 model = Sequential()
 
 model.add(Convolution2D(32, 3, 3, border_mode='same',
-                        input_shape=(img_channels, img_rows, img_cols), activation="relu", name="conv1"))
+                        input_shape=(img_channels, img_rows, img_cols),no_bias=True, activation="relu", name="conv1"))
+model.add(BatchNormalization())"""patillada """
+
 first_layer = model.layers[-1]
 # this is a placeholder tensor that will contain our generated images
 input_img = first_layer.input
-model.add(Convolution2D(32, 3, 3, activation="relu", name="conv2"))
+model.add(Convolution2D(32, 3, 3, activation="relu", no_bias=True,name="conv2"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 
-model.add(Convolution2D(64, 3, 3, activation="relu", name="conv3"))
-model.add(Convolution2D(64, 3, 3, activation="relu", name="conv4"))
+model.add(BatchNormalization())"""patillada """
+
+
+model.add(Convolution2D(64, 3, 3, activation="relu",no_bias=True, name="conv3"))
+
+model.add(BatchNormalization())"""patillada """
+
+model.add(Convolution2D(64, 3, 3, activation="relu",no_bias=True, name="conv4"))
+
+model.add(BatchNormalization())"""patillada """
+
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 
