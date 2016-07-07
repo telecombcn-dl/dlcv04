3d0ed6804afaee2b98e1904477f0f476895d18ad
hect1995 <hect.esteban@gmail.com>
2016-07-07 11:24:48 +0200

    Update plot_loss.py

--- task2/plot_loss.py
+++ task2/plot_loss.py
@@ -2,10 +2,16 @@ import matplotlib.pyplot as plt
 from matplotlib.legend_handler import HandlerLine2D
 
 log = raw_input("Enter the path:")
+log1 = raw_input("Enter the path:")
 loss=[]
 val_loss=[]
 acc=[]
 val_acc=[]
+loss1=[]
+val_loss1=[]
+acc1=[]
+val_acc1=[]
+
 
 for line in open(log).readlines():
 	aux_loss = 0
@@ -25,7 +31,25 @@ for line in open(log).readlines():
 
 name = log.split('.')
 
-plt.figure(1)
+for line in open(log1).readlines():
+	aux_loss = 0
+	aux_acc = 0
+	nline = line.split(' - ')
+	for l in nline:
+		if l.startswith('loss: '):
+			aux_loss=l[6:11]
+		elif l.startswith('acc: '):
+			aux_acc=l[5:10]
+		elif l.startswith('val_loss: '):
+			val_loss1.append(l[10:16])
+			loss1.append(aux_loss)
+		elif l.startswith('val_acc: '):
+			val_acc1.append(l[9:15])
+			acc1.append(aux_acc)
+
+name = log.split('.')
+
+"""plt.figure(1)
 x=range(1,len(loss)+1)
 plt.plot(x,loss, marker="x", label="Training Loss")
 plt.plot(x,val_loss, marker="x", label="Validation Loss")
@@ -35,11 +59,13 @@ plt.xlabel('Epoch')
 plt.ylabel('Loss')
 #plt.show()
 plt.savefig("plots/" + name[0] + "_loss.png")
-
+"""
 plt.figure(2)
 x=range(1,len(acc)+1)
-plt.plot(x,acc, marker="x", label="Training Accuracy")
-plt.plot(x,val_acc, marker="x", label="Validation Accuracy")
+plt.plot(x,acc, marker="x", label="Training Accuracy_32")
+plt.plot(x,val_acc, marker="x", label="Validation Accuracy_32")
+plt.plot(x,acc1, marker="x", label="Training Accuracy_128")
+plt.plot(x,val_acc1, marker="x", label="Validation Accuracy_128")
 plt.legend(loc="lower right")
 plt.xlim([1.0,len(acc)])
 plt.xlabel('Epoch')
